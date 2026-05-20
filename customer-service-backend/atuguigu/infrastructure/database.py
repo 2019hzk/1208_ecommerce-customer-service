f7e7e2342f87d83569be0cf1cb49435e9a81f223
huzhongkui <2595753724@qq.com>
2026-05-20 16:11:40 +0800

    day02_domain层设计

--- customer-service-backend/atuguigu/infrastructure/database.py
+++ customer-service-backend/atuguigu/infrastructure/database.py
@@ -6,6 +6,40 @@ sqlalchemy:名字sql log 幂
 操作自己的数据库（customer_service） dialogue_states表（整个对话状态：聚合根）
 
 """
+import asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker, AsyncSession
+from atuguigu.config.config import settings
+from sqlalchemy import text
+
+engine: AsyncEngine | None = None
+async_session: async_sessionmaker[AsyncSession]
 
 
+def init_db_engin():
+    global engine, async_session
+    # 1. 创建异步引擎
+    engine = create_async_engine(
+        settings.database_url,
+        echo=True)
 
+    # 2. 创建异步session工厂
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def close_db_engine():
+    await engine.dispose()
+
+
+async def main():
+    init_db_engin()
+
+    async  with async_session() as session:
+        result = await session.execute(text("select 1"))  # 测试是否通（core）
+        # print(type(result.fetchone()))
+        print(result.fetchone())
+
+    await close_db_engine()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
